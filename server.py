c19c5ee575e005965907528de1bc7d3ce9c5ec66
OumarVivienZX <KonanOumarvivien@gmail.com>
2025-10-23 10:58:36 +0000

    ajout du plan de test

--- server.py
+++ server.py
@@ -2,14 +2,19 @@ import json
 from flask import Flask, render_template, request, redirect, flash, url_for
 
 
-#le with ouvre le fichier clubs.json en mode lecture et le charge avec json.load
-#le json.load lit le contenu du fichier et le convertit en un objet Python (ici, un dictionnaire)
-#ensuite on accede a la cle "clubs" du dictionnaire pour obtenir la liste des clubs
+
+
+app = Flask(__name__)  # création de l'application Flask
+app.secret_key = "something_special"
+# clé secrète utilisée par Flask pour sécuriser les sessions et les cookies
+# le with ouvre le fichier clubs.json en mode lecture et le charge avec json.load
+# le json.load lit le contenu du fichier et le convertit en un objet Python (ici, un dictionnaire)
+# ensuite on accede a la cle "clubs" du dictionnaire pour obtenir la liste des clubs
+
 def loadClubs():
     with open("clubs.json") as c:
         listOfClubs = json.load(c)["clubs"]
-        return listOfClubs 
-    
+        return listOfClubs
 
 
 def loadCompetitions():
@@ -27,25 +32,30 @@ def saveCompetitions(competitions):
     with open("competitions.json", "w") as f:
         json.dump({"competitions": competitions}, f)
 
-app = Flask(__name__)# création de l'application Flask
-app.secret_key = "something_special"
-# clé secrète utilisée par Flask pour sécuriser les sessions et les cookies
-
-competitions = loadCompetitions()#  chargement des compétitions contienues dans le fichier competitions.json
-clubs = loadClubs()# chargement des clubs contenus dans le fichier clubs.json
 
 
-@app.route("/")# route principale de l'application
+
+competitions = (
+    loadCompetitions()
+)  #  chargement des compétitions contienues dans le fichier competitions.json
+clubs = loadClubs()  # chargement des clubs contenus dans le fichier clubs.json
+
+
+@app.route("/")  # route principale de l'application
 def index():
     return render_template("index.html")
 
 
-@app.route("/showSummary", methods=["POST"])# route pour afficher le résumé des compétitions et des clubs
+@app.route(
+    "/showSummary", methods=["POST"]
+)  # route pour afficher le résumé des compétitions et des clubs
 def showSummary():
     # club = [club for club in clubs if club["email"] == request.form["email"]][0]
     # return render_template("welcome.html", club=club, competitions=competitions)
 
-    matching_clubs = [club for club in clubs if club["email"] == request.form["email"]]# liste des clubs dont l'email correspond à celui soumis dans le formulaire
+    matching_clubs = [
+        club for club in clubs if club["email"] == request.form["email"]
+    ]  # liste des clubs dont l'email correspond à celui soumis dans le formulaire
     if matching_clubs:
         club = matching_clubs[0]
         return render_template("welcome.html", club=club, competitions=competitions)
@@ -63,7 +73,9 @@ def book(competition, club):
             "booking.html", club=foundClub, competition=foundCompetition
         )
     else:
-        flash("Nous rencontrons des difficultés pour trouver le club ou la compétition, veuillez réessayer")
+        flash(
+            "Nous rencontrons des difficultés pour trouver le club ou la compétition, veuillez réessayer"
+        )
         return render_template("welcome.html", club=club, competitions=competitions)
 
 
@@ -84,8 +96,7 @@ def purchasePlaces():
 
 @app.route("/logout")
 def logout():
-    return redirect(url_for("index"))#redirect
-
+    return redirect(url_for("index"))  # redirect
 
 
 if __name__ == "__main__":
